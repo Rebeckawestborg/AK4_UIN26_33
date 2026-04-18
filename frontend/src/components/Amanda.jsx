d4183251d8fbbfd45106c65087d67f488066a6ac
ama6543 <amanda.j.slettli@gmail.com>
2026-04-18 15:37:43 +0200

    Begynte på komponentet mitt og layout komponent

---
+++ frontend/src/components/Amanda.jsx
@@ -0,0 +1,11 @@
+import {Link} from "react-router-dom"
+export default function Amanda(){
+    return(
+        <>
+        <h1>Amanda Jeanette Slettli</h1>
+        <img src="/profil_img/anonym" alt="profil bilde"/>
+        <Link>amandajs@hiof.no</Link>
+        <p>Jeg går på bachelorstudiet: Digitale medier og design</p>
+        </>
+    )
+}
---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,7 @@
+export default function Layout(){
+    return(
+        <header>
+            <h1>Gruppe 33</h1>
+        </header>
+    )
+}
