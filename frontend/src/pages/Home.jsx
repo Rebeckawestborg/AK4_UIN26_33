7bbd238a7cdc30f601efb138fc754bc0c43ab3a7
Rebeckawestborg <rwestborg@gmail.com>
2026-04-21 11:25:08 +0200

    laget eget komponent

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Amanda from '../components/Amanda'
+import Rebecka from '../components/Rebecka'
 import AssigmentList from '../components/AssigmentList'
 export default function Home(){
     return(
         <main>
             <Amanda/>
+            <Rebecka/>
             <AssigmentList/>
         </main>
     )
