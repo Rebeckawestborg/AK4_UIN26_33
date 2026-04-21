65b057809d24ea9df8c6c5367d970b3ada9c536c
SophiaPbh <143319358+SophiaPbh@users.noreply.github.com>
2026-04-21 17:47:49 +0200

    sophia

---
+++ frontend/src/components/Sophia.jsx
@@ -0,0 +1,16 @@
+import "./Sophia.css";
+
+export default function Sophia() {
+  return (
+    <>
+    <p>Sophia Philippa Bildsten Helseth</p>
+      <p>Min e-post: {" "}
+      <a href="mailto:sphelset@hiof.no">sphelset@hiof.no</a></p>
+      <p><strong>Sophia</strong></p>
+    <article className="om-meg-innhold">
+      <img src="philippasophia.png" alt="sophia" className="sophia-img"  />
+      <p><strong>Bachelorstudium i Digitale medier og design</strong></p>
+    </article>
+    </>
+  );
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Amanda from '../components/Amanda'
+import Sophia from '../components/Sophia'
 import AssigmentList from '../components/AssigmentList'
 export default function Home(){
     return(
         <main>
             <Amanda/>
+            <Sophia />
             <AssigmentList/>
         </main>
     )
