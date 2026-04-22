cbf6198307d6d6a8957e0c2ea8859b7391915d30
Rebeckawestborg <rwestborg@gmail.com>
2026-04-22 11:45:25 +0200

    Rebecka.css og container i Home.jsx

--- frontend/src/components/Rebecka.jsx
+++ frontend/src/components/Rebecka.jsx
@@ -1,12 +1,12 @@
 
 export default function Rebecka(){
     return(
-        <article>
+        <article className="rebecka-card">
             <h1>Rebecka Westborg</h1>
             <img src="/profil_img/illustrertbilde_rebecka.png" alt= "illustrert bilde av rebecka"/>
             <ul>
-                <li>Epost: <a href= "mailto: Rebeckw@hiof.no">Rebeckwhiof.no</a></li>
-                <li>Studie: Bachelor i Digitale medier og Design</li>
+                <li>Epost: <a href= "mailto: Rebeckw@hiof.no">rebeckw@hiof.no</a></li>
+                <li>Studie: Bachelor i digitale medier og design</li>
             </ul>
         </article>
     )
