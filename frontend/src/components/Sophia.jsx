000d78eed36eb1a6255856f08977ff928aee4967
SophiaPbh <143319358+SophiaPbh@users.noreply.github.com>
2026-04-27 11:24:10 +0200

    fjernet unødvendig div tag
    for å unngå div

--- frontend/src/components/Sophia.jsx
+++ frontend/src/components/Sophia.jsx
@@ -4,14 +4,14 @@ import sophiaImg from '../assets/philippasophia.png';
 export default function Sophia() {
   return (
     <>
+    <section>
     <article className="om-meg-innhold">
       <h2>Sophia Bildsten Helseth</h2>
       <img src={sophiaImg} alt="sophia" className="sophia-img" />
-      <div>
       <p>Epost: {" "} <a href="mailto:sphelset@hiof.no">sphelset@hiof.no</a></p>
-      </div>
       <p>Bachelorstudium i Digitale medier og design</p>
     </article>
+    </section>
       </>
   );
 }
