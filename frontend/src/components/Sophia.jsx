f9fdd7c9cc7fefbf2c02e6e89eb43499eeedffb5
SophiaPbh <143319358+SophiaPbh@users.noreply.github.com>
2026-04-27 10:41:15 +0200

    lagt inn i main
    sophia

---
+++ frontend/src/components/Sophia.jsx
@@ -0,0 +1,18 @@
+import '../style/sophia.css';
+import sophiaImg from '../assets/philippasophia.png';
+
+export default function Sophia() {
+  return (
+    <>
+    <article className="om-meg-innhold">
+      <img src={sophiaImg} alt="sophia" className="sophia-img" />
+      <p><strong>Sophia Bildsten Helseth</strong></p>
+      <p><strong>Bachelorstudium i Digitale medier og design</strong></p>
+      <div>
+      <p><strong>Min e-post: {" "}</strong></p>
+      <a href="mailto:sphelset@hiof.no"><strong>sphelset@hiof.no</strong></a>
+      </div>
+    </article>
+      </>
+  );
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import Amanda from '../components/Amanda'
 import Rebecka from '../components/Rebecka'
 import AssigmentList from '../components/AssigmentList'
+import Sophia from '../components/Sophia'
+
 export default function Home(){
     return(
         <main>
             <section className='container'>
             <Amanda/>
             <Rebecka/>
+            <Sophia />
             </section>
             <AssigmentList/>
         </main>
